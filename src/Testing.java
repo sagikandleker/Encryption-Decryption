140666e780054a065ec8a17b84ccc7e923e507cd
Sagi Kandleker <33149808+sagikandleker@users.noreply.github.com>
2017-12-16 17:38:45 +0200

    Testing code

---
+++ src/Testing.java
@@ -0,0 +1,25 @@
+
+public class Testing {
+
+	public static void main(String[] args) {
+		String a = "א";
+		char b = 'א';
+		char d = 1488;
+		char s = 1514;
+		System.out.println(d);
+		System.out.println(s);
+		
+		int KEY;
+		String DATA = "רוני";
+		for (int i = 0; i < 99999999; i++) {
+			if(DATA.charAt(0) > 1487 && DATA.charAt(0) < 1515) {
+				
+				
+			}
+			
+		}
+		
+		
+	}
+
+}
