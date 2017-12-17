27013e0d800885d434dced42c45b5d391c9c7e86
Sagi Kandleker <33149808+sagikandleker@users.noreply.github.com>
2017-12-17 02:03:09 +0200

    Added getFiles from folder and loop to search keys for the Encoded files.

---
+++ src/Hack_Keys.java
@@ -0,0 +1,65 @@
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+public class Hack_Keys {
+
+	public static String Bonus_Path = Main.Path+"Bonus\\";
+	public static String Bonus_File_Write = Bonus_Path+"Bonus_Key_List.csv";
+
+	/**
+	 * 
+	 * @throws IOException
+	 * @throws ParseException
+	 */
+	public static void getFiles() throws IOException, ParseException {
+
+		File folder = new File(Bonus_Path);
+		String[] files_Names = folder.list();
+
+		int counter = 0;
+		for(int i = 0; i < files_Names.length; i++) {
+			if(files_Names[i].startsWith("enc") && files_Names[i].endsWith(".txt")) {
+				Decoding_Keys(Bonus_Path,files_Names[i]);
+				counter++;
+			}	
+		}
+		
+		System.out.println(counter +" Files founded\nThe Keys to the files are in the file - "+Bonus_File_Write);
+	}
+
+	/**
+	 * 
+	 * @param Folder
+	 * @param File
+	 * @throws IOException
+	 */
+	public static void Decoding_Keys(String Folder, String File) throws IOException {
+		Write_2_CSV.Write_Headers();
+		ArrayList<String> Keys_List = new ArrayList<String>();
+
+		String encoded_data = Read_From_File.Integer_File(Folder+File);
+		String decoded_data;
+
+		//Thread t1=new Thread(DATA);
+		//Thread t2=new Thread(DATA);
+
+		//t1.start();
+		//t2.start();
+
+		for (int i = 0; i < 99999999; i++) {
+			decoded_data = Algorithm.Decryption(encoded_data, i);
+			if((decoded_data.charAt(0) > 1487 && decoded_data.charAt(0) < 1515) &&
+					(decoded_data.charAt(1) > 1487 && decoded_data.charAt(1) < 1515) &&
+					(decoded_data.charAt(2) > 1487 && decoded_data.charAt(2) < 1515)) {
+
+				System.out.println("File Name: "+File+"\nKey: "+i);
+				Keys_List.add(File);
+				Keys_List.add(""+i);
+				break;
+			}
+		}
+		Write_2_CSV.Write(Keys_List);
+	}
+}
