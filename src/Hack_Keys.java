d4b8edaebc1d5ac2d3f684cd42a19dc7ef55ae9e
Sagi Kandleker <33149808+sagikandleker@users.noreply.github.com>
2017-12-19 21:32:59 +0200

    Added nanoTime.

--- src/Hack_Keys.java
+++ src/Hack_Keys.java
@@ -1,20 +1,17 @@
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
-import java.time.Duration;
-import java.time.Instant;
 import java.util.ArrayList;
 
 /**
  * Bonus part.
- * @author Sagi, Roni, Shlomi.
  *
  */
 public class Hack_Keys {
 
 	public static String Bonus_Path = Main.PATH+"Bonus/";
 	public static String Bonus_File_Write = Bonus_Path+"Bonus_Key_List.csv";
-	
+
 	/**
 	 * Decode all files in bonus folder.
 	 * @throws IOException
@@ -22,7 +19,7 @@ public class Hack_Keys {
 	 * @throws InterruptedException
 	 */
 	public static void decodeFiles() throws IOException, ParseException, InterruptedException {
-
+		//final long startTime = System.nanoTime();
 		File folder = new File(Bonus_Path);
 		String[] files_Names = folder.list();
 		ArrayList<String> Keys_List = new ArrayList<String>();
@@ -36,6 +33,8 @@ public class Hack_Keys {
 			}	
 		}
 		Writer.Write_2_CSV(Keys_List);
+		//final long duration = System.nanoTime() - startTime;
+		//System.out.println(duration/1000000000);
 		System.out.println(counter +" Files founded\nThe Keys to the files are in the file - "+Bonus_File_Write);
 	}
 
